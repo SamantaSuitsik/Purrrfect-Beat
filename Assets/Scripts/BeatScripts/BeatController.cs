d3182854c6b0e55ea1ea2564e83383b9a5d4e5a4
DianaPri <diana980104@gmail.com>
2024-11-18 14:02:14 +0200

    Fixed BeatController
    fixed some issues

--- Assets/Scripts/BeatScripts/BeatController.cs
+++ Assets/Scripts/BeatScripts/BeatController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net;
 using UnityEngine;
 
 public class BeatController : MonoBehaviour
@@ -23,9 +22,13 @@ public class BeatController : MonoBehaviour
     public float BeatSpawnInterval;
     private float nextBeatTime;
 
+    private float[] spawnDelayList = { 2f, 3f, 4f, 6f, 9f };
+
+    // List to track active beats
+    private List<BeatScroller> activeBeats = new List<BeatScroller>();
+
     void Awake()
     {
-       
         if (Instance == null)
         {
             Instance = this;
@@ -51,27 +54,86 @@ public class BeatController : MonoBehaviour
 
     private void Update()
     {
-        if ((float)AudioSettings.dspTime >= nextBeatTime) //Check if it's time to spawn new beat
+        if ((float)AudioSettings.dspTime >= nextBeatTime) // Check if it's time to spawn a new beat
         {
             SpawnBeat();
             nextBeatTime += BeatSpawnInterval;
+            BeatSpawnInterval = SecPerBeat / spawnDelayList[Random.Range(0, spawnDelayList.Length)];
+        }
+
+        // Handle key press for the closest beat
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            HandleHit();
         }
     }
 
     private void SpawnBeat()
     {
-        // Creating beat on spawnpoint
+        // Creating beat at spawn point
         GameObject newBeat = Instantiate(BeatPrefab, SpawnPoint.position, Quaternion.identity);
 
-        
         BeatScroller beatScroller = newBeat.GetComponent<BeatScroller>();
+        activeBeats.Add(beatScroller); // Add to active beats list
 
         Debug.Log("The new beat is spawned!");
+    }
+
+    private void HandleHit()
+    {
+        if (activeBeats.Count > 0)
+        {
+            // Clean up null or destroyed beats from the list
+            activeBeats.RemoveAll(beat => beat == null);
+
+            // Check again after cleaning up
+            if (activeBeats.Count == 0) return;
+
+            // Find the closest beat to the hit point
+            BeatScroller closestBeat = activeBeats[0];
+            float minDistance = Vector2.Distance(closestBeat.transform.position, HitPoint.position);
+
+            foreach (var beat in activeBeats)
+            {
+                float distance = Vector2.Distance(beat.transform.position, HitPoint.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestBeat = beat;
+                }
+            }
+
+            // Check hit/miss conditions
+            float hitDistance = Vector2.Distance(closestBeat.transform.position, HitPoint.position);
+            if (hitDistance < 0.2f)
+            {
+                Debug.Log("Ultra hit!");
+                HitPoint.GetComponent<EndPointController>().OnHit();
+            }
+            else if (hitDistance < 1f)
+            {
+                Debug.Log("Successful hit!");
+                HitPoint.GetComponent<EndPointController>().OnHit();
+            }
+            else
+            {
+                Debug.Log("Missed hit!");
+                HitPoint.GetComponent<EndPointController>().OnMiss();
+            }
+
+            // Remove and destroy the closest beat
+            activeBeats.Remove(closestBeat);
+            Destroy(closestBeat.gameObject);
+        }
+    }
+
+    public void RemoveBeat(BeatScroller beat)
+    {
+        activeBeats.Remove(beat); // Remove the beat from the active list
     }
 
     public static float GetSongPositionInBeats()
     {
         return ((float)AudioSettings.dspTime - Instance.DspSongTime) / SecPerBeat;
     }
-
 }
--- Assets/Scripts/BeatScripts/BeatScroller.cs
+++ Assets/Scripts/BeatScripts/BeatScroller.cs
@@ -9,7 +9,6 @@ public class BeatScroller : MonoBehaviour
     private Transform hitPoint;
 
     private float moveSpeed;
-    //private bool hasTriggeredEndPoint = false;
 
     void Start()
     {
@@ -17,50 +16,24 @@ public class BeatScroller : MonoBehaviour
         endPoint = BeatController.Instance.EndPoint;
         hitPoint = BeatController.Instance.HitPoint;
 
-        // Distance between startpoint(spawn) and endpoint
-        float distance = Vector2.Distance((spawnPoint.position), endPoint.position);
+        // Distance between start point (spawn) and endpoint
+        float distance = Vector2.Distance(spawnPoint.position, endPoint.position);
 
         moveSpeed = distance / BeatController.SecPerBeat;
     }
 
-    
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, endPoint.position, moveSpeed * Time.deltaTime);
 
-
-        // check if beat reached endpoint
+        // Check if beat reached endpoint
         if (Vector2.Distance(transform.position, endPoint.position) == 0)
         {
-
             hitPoint.GetComponent<EndPointController>().OnMiss();
+
+            // Notify BeatController and destroy the beat
+            BeatController.Instance.RemoveBeat(this);
             Destroy(gameObject);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-
-            if (Vector2.Distance(transform.position, hitPoint.position) < 0.2f)
-            {
-                Debug.Log("Ultra hit!");
-
-                hitPoint.GetComponent<EndPointController>().OnHit();
-            }
-            else if (Vector2.Distance(transform.position, hitPoint.position) < 1f)
-            {
-                Debug.Log("Successful hit!");
-
-                hitPoint.GetComponent<EndPointController>().OnHit();
-            }
-            else
-            {
-                Debug.Log("Missed hit!");
-
-                hitPoint.GetComponent<EndPointController>().OnMiss();
-            }
-            Destroy(gameObject);
-        }
-
-
-        
     }
 }
